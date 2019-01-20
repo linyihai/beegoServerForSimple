8dc4cfd32d854a3101bced224f0888cc773652d5
LinYiHai <1161813899@qq.com>
2019-01-21 01:28:31 +0800

    first commit, some demo complete

---
+++ jianyifundserver/routers/router.go
@@ -0,0 +1,12 @@
+package routers
+
+import (
+	"jianyifundserver/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/userContract", &controllers.UserContractController{}, "*:Post")
+}
