bf330385083e101b32f55f3ec1742ed368800a7f
linyihai <1161813899@qq.com>
2019-02-18 19:33:54 +0800

    增加跨域支持

--- src/jianyifundserver/main.go
+++ src/jianyifundserver/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	_ "jianyifundserver/routers"
-
+	"github.com/astaxie/beego/plugins/cors"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods: []string{"PUT", "POST", "GET", "DELETE", "OPTIONS"},        
+		AllowHeaders: []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders: []string{"Content-Length"},     
+		AllowCredentials: true, })) 
 	beego.BConfig.Listen.Graceful=false
 	beego.BConfig.Listen.HTTPPort = 8080
 	beego.SetLogFuncCall(true)
